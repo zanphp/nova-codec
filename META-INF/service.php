7961ce1b3c97e16e103fff49b81665354b21fa68
chuxiaofeng <chuxiaofeng@youzan.com>
2017-07-27 15:46:37 +0800

    add service

--- META-INF/service.php
+++ META-INF/service.php
@@ -1,5 +1,9 @@
 <?php
 
 return [
-
+    \ZanPHP\NovaCodec\NovaCodec::class => [
+        "interface" => \ZanPHP\Contracts\ServiceChain\ServiceChainer::class,
+        "id" => "codec:nova",
+        "shared" => true,
+    ],
 ];
