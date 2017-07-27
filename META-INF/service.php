09bf3abc191e73b8984bea9ad59369e96256eaac
chuxiaofeng <chuxiaofeng@youzan.com>
2017-07-27 16:09:23 +0800

    fix interface

--- META-INF/service.php
+++ META-INF/service.php
@@ -2,7 +2,7 @@
 
 return [
     \ZanPHP\NovaCodec\NovaCodec::class => [
-        "interface" => \ZanPHP\Contracts\ServiceChain\ServiceChainer::class,
+        "interface" => \ZanPHP\Contracts\Codec\Codec::class,
         "id" => "codec:nova",
         "shared" => true,
     ],
